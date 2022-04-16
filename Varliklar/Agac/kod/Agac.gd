48b7c3d215db4f5be1ce249c3c353ae4567158a6
Xtero61 <oakay626@gmail.com>
2022-04-16 19:45:59 +0300

    Ağaç animasyonları düzeltildi.

--- Varliklar/Agac/kod/Agac.gd
+++ Varliklar/Agac/kod/Agac.gd
@@ -1,82 +1,28 @@
 extends Node2D
-onready var animationPlayer = $AnimationPlayer
-export var vurulmaSayi = 0
-var yikilmaSayi = 15
-var Sayi = 1
-var sag = 0
-var sol = 0
-var ust = 0
-var alt = 0
+
+onready var oyuncu: Oyuncu = get_node(Genel.OYUNCU_YOLU)
+onready var animasyon_agaci: AnimationTree = $AnimationTree
+onready var animasyon_oynatici: AnimationPlayer = $AnimationPlayer
+onready var animasyon_durumu = animasyon_agaci.get("parameters/playback")
+export var vurulmaSayi: int = 0
+const yikilmaSayi: int = 15
+var Sayi: int = 1
+var hareket_vektoru := Vector2.ZERO
 
 func _process(_delta):
-
-    if vurulmaSayi ==  Sayi :
-        animationPlayer.play("Vurulma")
-        Sayi +=  1
-
-    if vurulmaSayi == yikilmaSayi and (ust == 1 and sol == 1):
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSağ")
-    elif vurulmaSayi == yikilmaSayi and (alt == 1 and sol == 1):
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSağ")
-    elif vurulmaSayi == yikilmaSayi and (ust == 1 and sag == 1):
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSol")
-    elif vurulmaSayi == yikilmaSayi and (alt == 1 and sag == 1):
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSol")
-    elif vurulmaSayi == yikilmaSayi and ust == 1 :
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSağ")
-    elif vurulmaSayi == yikilmaSayi and sol == 1 :
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSağ")
-    elif vurulmaSayi == yikilmaSayi and alt == 1 :
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSol")
-    elif vurulmaSayi == yikilmaSayi and sag == 1 :
-        animationPlayer.stop()
-        animationPlayer.play("KesilmeSol")
+    if vurulmaSayi == yikilmaSayi :
+        hareket_vektoru = oyuncu.getir_hareket_vektoru()
+        animasyon_agaci.set("parameters/Kesildim/blend_position", hareket_vektoru)
+        animasyon_durumu.travel("Kesildim")
+        vurulmaSayi = 0
 
 func _Kesildim():
     queue_free()
     Arac.esya_dusur(Genel.ESYA_ODUN, position)
 
-func _on_Sag_body_entered(body):
-    if body.name == "Oyuncu":
-        sag = 1
-
-func _on_Sag_body_exited(body):
-    if body.name == "Oyuncu":
-        sag = 0
-
-func _on_Sol_body_entered(body):
-    if body.name == "Oyuncu":
-        sol = 1
-
-func _on_Sol_body_exited(body):
-    if body.name == "Oyuncu":
-        sol = 0
-
-func _on_Ust_body_entered(body):
-    if body.name == "Oyuncu":
-        ust = 1
-
-func _on_Ust_body_exited(body):
-    if body.name == "Oyuncu":
-        ust = 0
-
-func _on_Alt_body_entered(body):
-    if body.name == "Oyuncu":
-        alt = 1
-
-func _on_Alt_body_exited(body):
-    if body.name == "Oyuncu":
-        alt = 0
-
-
 func _on_AgacAlan_area_entered(area):
-    if area.name == "AxeHit":
+     if area.name == "AxeHit":
+        animasyon_durumu.travel("Vurma")
         vurulmaSayi += 1
         $AudioStreamPlayer2D.play()
+
