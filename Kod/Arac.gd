5e22b64dd299baa7b3dd7aed1810630241bea329
akaybayram61 <akay.bayram.1461@gmail.com>
2022-05-01 18:28:22 +0300

    onceki komitin devamı

--- Kod/Arac.gd
+++ Kod/Arac.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 # Araçlar
-func rastgele_saymasayisi(ust_sinir: int):
+func rastgele_saymasayisi(ust_sinir: int) -> int:
     return (randi() % ust_sinir) + 1
+    
+func rastgele_noktali_sayi(ust_sinir: float) -> float:
+    return (randf() * ust_sinir)
 
 func getir_ysort():
     return get_node(Genel.YERYUZU_YOLU)
@@ -10,7 +13,7 @@ func getir_ysort():
 func getir_oyuncu():
     return get_node(Genel.OYUNCU_YOLU)
 
-func esya_dusur(esya: int, dusme_yeri: Vector2):
+func esya_dusur(esya: int, dusme_yeri: Vector2) -> void:
     var adet = Arac.rastgele_saymasayisi(Genel.esya[esya][Genel.ESYA_MAKS_DUSME])
     for i in adet:
         var olusan_esya: Esya = Genel.esya[esya][Genel.ESYA_SAHNE].instance()
@@ -18,3 +21,14 @@ func esya_dusur(esya: int, dusme_yeri: Vector2):
         getir_ysort().add_child(olusan_esya)
         olusan_esya.dusme_hareketi_baslat(dusme_yeri)
         olusan_esya.rastgele_kuvvet_uygula()
+        
+func zar_at() -> int:
+    return rastgele_saymasayisi(6)
+
+func getir_yuzde_sans(yuzde: int) -> bool:
+    return (rastgele_saymasayisi(100) > yuzde)
+    
+func rastgele_yuzdeyle_sinirla(sayi: float, alt_yuzde: int, ust_yuzde: int):
+    var alt_sinir := sayi * (alt_yuzde / 100.0)
+    var ust_sinir := sayi * (ust_yuzde / 100.0)
+    return (rastgele_noktali_sayi(ust_sinir - alt_sinir) + alt_sinir)
--- Kod/Genel.gd
+++ Kod/Genel.gd
@@ -1,8 +1,8 @@
 extends Node2D
 # Genel değsişken tanımlamaları
 
-const MAKS_ESYA_MIKTARI : int = 100
 const ESYA_DUSME_RASTGELE_KUVVET : int = 500
+const ESYA_DUSME_MAKS_DONUS_KUVVETI = 5000
 const OYUNCU_YOLU: String = "/root/Dunya/Yeryuzu/Oyuncu"
 const YERYUZU_YOLU: String = "/root/Dunya/Yeryuzu"
 const DUNYA_OLCEGI: Vector2 = Vector2(2, 2)
@@ -25,7 +25,8 @@ enum{
 enum{
     ESYA_ID,
     ESYA_MAKS_DUSME,
-    ESYA_SAHNE
+    ESYA_MAKS_YIGIN_ADET,
+    ESYA_SAHNE,
 }
 
 enum{
@@ -35,11 +36,11 @@ enum{
 
 # Esya yüklemeleri
 const esya = [
-    # ESYA_ID     , MAKS_DUSME  , ESYA_SAHNE
-    [ ESYA_BALTA  , 1           , preload("res://Esyalar/Balta/Balta.tscn")],
-    [ ESYA_KAZMA  , 1           , preload("res://Esyalar/Kazma/Kazma.tscn")],
-    [ ESYA_ODUN   , 4           , preload("res://Esyalar/Odun/Odun.tscn")  ],
-    [ ESYA_TAS    , 4           , preload("res://Esyalar/Tas/Tas.tscn")    ],
+    # ESYA_ID     , MAKS_DUSME, MAKS_YIGIN_ADET  , ESYA_SAHNE                               
+    [ ESYA_BALTA  , 1         , 1                , preload("res://Esyalar/Balta/Balta.tscn")],
+    [ ESYA_KAZMA  , 1         , 1                , preload("res://Esyalar/Kazma/Kazma.tscn")],
+    [ ESYA_ODUN   , 4         , 99               , preload("res://Esyalar/Odun/Odun.tscn")  ],
+    [ ESYA_TAS    , 4         , 99               , preload("res://Esyalar/Tas/Tas.tscn")    ],
 ]
 
 # Varlik yuklemeleri
